90876eb4f4de8ad5afbe2a6a120c10385a889a01
thi2u <thiago.jsp02@gmail.com>
2025-06-28 12:46:52 +0000

    Correção na ativação de variáveis Percebi que esqueci de ativar grupos de variáveis para as 2 cartas. Afinal, se serão 2 cartas então irei precisar de 2 grupos de variáveis para armazenar os valores para as 2 cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,24 +7,36 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    char codigo_cidade[2], nome[15], estado; //Definição das variáveis relacionadas ao codigo e ao nome da cidade, respectivamente, e a variável para o nome do estado.
-    int populacao, num_pnt_turisticos; //Definição das variáveis para quant. populacional e total de pontos turísticos, respectivamente.
-    float area, pib; //Definição das variáveis de área por km² e PIB, definidas como ponto flutuante por se referirem tanto a inteiros quanto racionais.
     
+    /*      Definição das variáveis para a carta 1      */
+    char codigo_cidade1[2], nome1[15], estado1; //Variáveis relacionadas ao codigo e ao nome da cidade, respectivamente, e a variável para o nome do estado.
+    int populacao1, num_pnt_turisticos1; //Variáveis para quant. populacional e total de pontos turísticos, respectivamente.
+    float area1, pib1; //Variáveis de área por km² e PIB.
+    /*                      --x--                       */
+
+    /*      Definição das variáveis para a carta 2      */
+    char codigo_cidade2[2], nome2[15], estado2; 
+    int populacao2, num_pnt_turisticos2;
+    float area2, pib2; 
+    /*                      --x--                       */
+
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     printf("Inicializando o jogo:\nSuper Trunfo de Países\n\nSessão: Cadastro de cartas\n\n");
     printf("Bem-vindo. Nesta sessão do jogo se cadastram as cartas que serão utilizadas no jogo Super Trunfo Países.\n\n");
     printf("Conto com sua participação para responder as seguintes perguntas a fim de cadastrar 2 novas cartas.\nVamos lá!\n\n"); //Trecho de carater simulatório, para gerar tanto entendimento quanto a atenção do usuário para os inputs subsequentes
+    printf("Vamos começar com a Carta 1:\n\n");
     printf("Primeiramente vamos ao nome do Estado.\nNeste jogo existem 8 estados, listados de A a H.\n\nPara cadastrar uma nova carta escolha um estado de A a H: ");
-    scanf("%c", &estado);
-    printf("\nVejo que escolheu o estado %c, então agora quero que escolha a cidade.\n", estado);
+    scanf("%c", &estado1);
+    printf("\nVejo que escolheu o estado %c, então agora quero que escolha a cidade.\n", estado1);
     printf("Saiba que os estados se dividem em 4 cidades, listados de 01 a 04, incluindo o zero a esquerda.\n\n");
     printf("Portanto escolha um número de 01 a 04: ");
-    scanf("%s", codigo_cidade);
+    scanf("%s", codigo_cidade1);
+    
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
